753a72209d01239d0fb8aa112cf1522c0ed8ba63
Chaithanya-Kumar-N <nchaithanyakumar868@gmail.com>
2022-04-28 21:40:38 +0530

    host modify

---
+++ backend/app/routes/app.routes.js
@@ -0,0 +1,16 @@
+const controller = require("../controllers/app.controller");
+const { authJwt } = require("../middleware");
+module.exports = function(app) {
+    app.use(function(req, res, next) {
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+    });
+    app.get("/api/app/:appid", [authJwt.verifyToken], controller.oneApp);
+    app.put("/api/app/:appid", [authJwt.verifyToken, authJwt.isAdmin], controller.modifyApp);
+    app.delete("/api/app/:appid", [authJwt.verifyToken, authJwt.isAdmin], controller.deleteApp);
+    app.get("/api/app", [authJwt.verifyToken], controller.allApp);
+    app.post("/api/app", [authJwt.verifyToken, authJwt.isAdmin], controller.addApp);
+};
