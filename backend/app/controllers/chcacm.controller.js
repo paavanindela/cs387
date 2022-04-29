652247bd1bb3538ad7773de930af49998169b65d
Chaithanya-Kumar-N <nchaithanyakumar868@gmail.com>
2022-04-29 23:41:03 +0530

    chcacm.controller

--- backend/app/controllers/chcacm.controller.js
+++ backend/app/controllers/chcacm.controller.js
@@ -8,37 +8,15 @@ exports.allChcacm = (req, res) => {
         'mlist': null
     };
 
-    User.findOne(req.userId).then(user => {
-        if(!user.rows[0]){
-            res.status(200).send({message: "user doesn't exist"});
-        }
-        else if (user.rows[0].status == 2) {
-            Chcacm.getchall().then(row1 => {
-                output['hlist'] = row1;
-                Chcacm.getcaall().then(row2 => {
-                    output['alist'] = row2;
-                    Chcacm.getcmall().then(row3 => {
-                        output["mlist"] = row3;
-                        res.status(200).send(output);
-                    });
-                });
+    Chcacm.getch(req.body.username).then(row1 => {
+        output['hlist'] = row1;
+        Chcacm.getca(req.body.username).then(row2 => {
+            output['alist'] = row2;
+            Chcacm.getcm(req.body.username).then(row3 => {
+                output["mlist"] = row3;
+                res.status(200).send(output);
             });
-        }
-        else if (user.rows[0].status == 0) {
-            res.status(200).send({message: "user inactive"});
-        }
-        else{
-            Chcacm.getch(req.userId).then(row1 => {
-                output['hlist'] = row1;
-                Chcacm.getca(req.userId).then(row2 => {
-                    output['alist'] = row2;
-                    Chcacm.getcm(req.userId).then(row3 => {
-                        output["mlist"] = row3;
-                        res.status(200).send(output);
-                    });
-                });
-            });
-        }
+        });
     }).catch(
         err => {
             res.status(500).send({message: err.message});
@@ -48,9 +26,9 @@ exports.allChcacm = (req, res) => {
 
 exports.addChcacm = (req, res) => {
     console.log(req.body);
-    Chcacm.addch(req.body.hlist, req.userId).then(() => {
-        Chcacm.addca(req.body.alist, req.userId).then(() => {
-            Chcacm.addcm(req.body.mlist, req.userId).then(() => {
+    Chcacm.addch(req.body.hlist, req.body.username).then(() => {
+        Chcacm.addca(req.body.alist, req.body.username).then(() => {
+            Chcacm.addcm(req.body.mlist, req.body.username).then(() => {
                 res.status(201).send({message: "changes made successfully"});
             });
         });
